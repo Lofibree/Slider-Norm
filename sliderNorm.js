ae5ab15c7a227e90d219a5d4301a5f63151f4a24
Lofibree <nastenkapersik17@gmail.com>
2022-10-04 12:41:44 +0300

    third commit

--- sliderNorm.js
+++ sliderNorm.js
@@ -2,10 +2,8 @@ var buttonRightEl = document.getElementById('left');
 var buttonLeftEl = document.getElementById('right');
 var buttonStartEl = document.getElementById('play');
 var buttonStopEl = document.getElementById('stop');
-var dots = document.getElementsByClassName('dot');
 var intervalID;
 var x = 0;
-var i = 0;
 var timer;
 buttonLeftEl.onclick = sliderLeft;
 buttonRightEl.onclick = sliderRight;
@@ -17,10 +15,7 @@ function sliderLeft() {
     x = x - 280;
     if (x < -2520) {
         x = 0;
-    }
-    var i = Math.trunc(-x/280);
-    dots[i].className += ' active';
-    dots[i-1].className = dots[i-1].className.replace(' active', '');
+    };
     sliderBoxEl.style.left = x + 'px';
 }
 function sliderRight() {
@@ -33,11 +28,11 @@ function sliderRight() {
 }
 function sliderAuto() {
     var sliderBoxEl = document.getElementById('slider-box');
-        x = x - 280;
-        if (x < -2212) {
-            x = 0;
-            clearTimeout(timer);
-        }
+    x = x - 280;
+    if (x < -2212) {
+        x = 0;
+        clearTimeout(timer);
+    }
     sliderBoxEl.style.left = x + 'px';
     sliderStart();
 }
